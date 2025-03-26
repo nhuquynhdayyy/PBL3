5e7c05c473732516d4ef8ec42742653905414044
Oanh <chuyenphiemvc@gmail.com>
2025-03-26 20:09:10 +0700

    thay doi co them login,res vao 8h ngay 26/3

--- obj/Debug/net9.0/TourismWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/TourismWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c43210e6745797fa59c5ca862fb00a21cf9002c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2a94d66a4d7d4633026637451a3a420f8da20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourismWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
